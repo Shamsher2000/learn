eeb942f81ca6a8b6cf5b59696af94559b6fdad31
Shamsher2000 <Shamshertiwari.nov2000@gmail.com>
2024-05-18 23:35:05 +0530

    Adding junk

---
+++ str.py
@@ -0,0 +1,10 @@
+# s={"w":"it is important message"}
+# print(s["w"])
+# print("good morning")
+# print(not False)
+# print(2+int("2"))
+
+
+name=input("enter you name")
+int(name)
+print("hello",name)
